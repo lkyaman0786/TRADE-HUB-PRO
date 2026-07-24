594606f92acac4088be7ac256b3a0a060045d5b2
lkyaman0786 <lkyaman@gmail.com>
2026-07-24 12:32:37 +0530

    Add CORS headers to Flask backend for API requests

--- algo.py
+++ algo.py
@@ -2897,6 +2897,13 @@ def init_app_engine():
 init_app_engine()
 
 
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
+
 @app.route('/')
 def home():
     if os.path.exists('index.html'):
